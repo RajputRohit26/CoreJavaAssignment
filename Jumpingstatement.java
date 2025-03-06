3f2c29a8ff3843a51831669b9a350f91fedbe8ce
RajputRohit26 <mrraaj083@gmail.com>
2025-03-06 22:09:23 +0530

    06-03-2025

---
+++ Jumpingstatement.java
@@ -0,0 +1,20 @@
+package com.basic;
+
+public class Jumpingstatement {
+	public static void main(String[] args) {
+		int i;
+		for(i=0;i<10;i++)
+		{
+			if(i==5) 
+			{
+				break;
+			}
+			else
+			{
+				System.out.println("I :"+i);
+			}
+			
+		}
+	}
+
+}
